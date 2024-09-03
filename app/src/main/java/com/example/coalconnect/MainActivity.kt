55671a77b80d1185b7a9d0c562179c8deba443b1
abhijeetsharma016 <abhijeetsharma16102004@gmail.com>
2024-09-04 02:49:22 +0530

    added navigation to the app

--- app/src/main/java/com/example/coalconnect/MainActivity.kt
+++ app/src/main/java/com/example/coalconnect/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.coalconnect
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.coalconnect.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var NavController = findNavController(R.id.fragmentContainerView2)
+        var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav.setupWithNavController(NavController)
+        binding.notificationButton.setOnClickListener{
+        }
 
     }
 }
